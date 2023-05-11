6851cd932c9c0d56cce2664c16f690e2521386e8
McastIII <jeancapunocapuno@gmail.com>
2023-05-11 15:47:44 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,34 @@
+// toggle menu
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__mobile");
+const blurr = document.querySelector(".blur");
+toggleMenu.addEventListener("click", () => {
+    toggleMenu.classList.toggle("open");
+    mobileNav.classList.toggle("open");
+    blurr.classList.toggle("open");
+});
+
+// dropdown
+const links = document.querySelectorAll(".links__drop__item h4");
+
+links.forEach((baba) => {
+    baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+    });
+});
+
+
+// // header scroll
+// const header = document.querySelector(".header");
+// const brandimg = document.querySelector(".branding img");
+// window.addEventListener("scroll", () => {
+//     console.log(window.pageYOffset);
+//     if (window.pageYOffset >= 100) {
+//     header.classList.add("blue");
+//     brandimg.style = "background-color:red";
+//     } else {
+//     header.classList.remove("blue");}
+
+// });
+
